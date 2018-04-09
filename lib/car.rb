cad539b4271de80c643764424ee1a1971dd86fcf
Hiroki Kato <hkato@sas.upenn.edu>
2018-04-09 20:19:49 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,4 +2,7 @@ require_relative "./vehicle.rb"
 #require relative so that our Car class can access Vehicle class
 
 class Car < Vehicle
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
 end
